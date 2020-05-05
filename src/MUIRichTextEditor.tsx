0c227a0188b44060c28edf8b5f51a969dce25681
Erik Lopez <niuware@users.noreply.github.com>
2020-05-06 00:05:41 +0900

    Fix clear styles (#117)
    * Fix typo
    
    * Add custom styles to clearInlineStyles function
    
    * Implement updated function
    
    * Update package version

--- src/MUIRichTextEditor.tsx
+++ src/MUIRichTextEditor.tsx
@@ -159,7 +159,7 @@ const blockRenderMap = Immutable.Map({
     }
 })
 const styleRenderMap: DraftStyleMap = {
-    'STRIKETROUGH': {
+    'STRIKETHROUGH': {
         textDecoration: "line-through"
     },
     'HIGHLIGHT': {
@@ -487,7 +487,7 @@ const MUIRichTextEditor: RefForwardingComponent<any, IMUIRichTextEditorProps> = 
     }
 
     const handleClearFormat = () => {
-        const withoutStyles = clearInlineStyles(editorState)
+        const withoutStyles = clearInlineStyles(editorState, customRenderers.style)
         const selectionInfo = getSelectionInfo(editorState)
         const newEditorState = EditorState.push(editorState, withoutStyles, 'change-inline-style');
         setEditorState(RichUtils.toggleBlockType(newEditorState, selectionInfo.blockType))
