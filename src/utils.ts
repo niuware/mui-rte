5352944a7ee9bcdf726d12d5843675bb8f59d0e3
Erik Lopez <maileks@gmail.com>
2020-05-06 00:04:11 +0900

    Add custom styles to clearInlineStyles function

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { EditorState, DraftBlockType, ContentBlock, ContentState, 
-    Modifier, SelectionState, getVisibleSelectionRect } from 'draft-js'
+    Modifier, SelectionState, getVisibleSelectionRect, DraftStyleMap } from 'draft-js'
 import Immutable from 'immutable'
 import { TCustomControl } from './components/Toolbar'
 
@@ -74,8 +74,11 @@ const isGt = (value: number, maxValue?: number): boolean => {
     return value > maxValue
 }
 
-const clearInlineStyles = (editorState: EditorState): ContentState => {
-    const styles = ['BOLD', 'ITALIC', 'UNDERLINE', 'STRIKETHROUGH']
+const clearInlineStyles = (editorState: EditorState, customStyles?: DraftStyleMap): ContentState => {
+    let styles = ['BOLD', 'ITALIC', 'UNDERLINE']
+    if (customStyles) {
+        styles = styles.concat(Object.getOwnPropertyNames(customStyles))
+    }
     return styles.reduce((newContentState: ContentState, style: string) => (
         Modifier.removeInlineStyle(newContentState, editorState.getSelection(), style)
     ), editorState.getCurrentContent())
