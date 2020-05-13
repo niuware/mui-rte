70c4af76af0dc5c6f83f14ee5998111dcccba951
Erik Lopez <maileks@gmail.com>
2020-05-13 11:47:12 +0900

    Add async image upload example

--- examples/main.tsx
+++ examples/main.tsx
@@ -16,6 +16,7 @@ import KeyBindings from './key-bindings'
 import MaxLength from './max-length'
 import Autocomplete from './autocomplete'
 import AutocompleteAtomic from './autocomplete-atomic'
+import AsyncImageUpload from './async-image-upload'
 import AsyncAtomicCustomBlock from './async-atomic-custom-block'
 
 const App = () => {
@@ -38,6 +39,7 @@ const App = () => {
             <button onClick={() => setSample(<CustomInlineToolbar />)}>Custom Inline Toolbar</button>
             <button onClick={() => setSample(<AtomicCustomBlock />)}>Atomic Custom Block</button>
             <button onClick={() => setSample(<AsyncAtomicCustomBlock />)}>Async Atomic Custom Block</button>
+            <button onClick={() => setSample(<AsyncImageUpload />)}>Async image upload</button>
             <button onClick={() => setSample(<ReadOnly />)}>Read Only</button> 
             <button onClick={() => setSample(<Events />)}>Events</button>
             <button onClick={() => setSample(<LoadHTML />)}>Load from HTML</button>
