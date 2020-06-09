af9fbd4158004a517282ba725ff97da8f1fdede7
Erik Lopez <maileks@gmail.com>
2020-06-09 11:38:45 +0900

    Fix popover dismiss when a link is confirmed

--- src/MUIRichTextEditor.tsx
+++ src/MUIRichTextEditor.tsx
@@ -738,7 +738,7 @@ const MUIRichTextEditor: RefForwardingComponent<TMUIRichTextEditorRef, IMUIRichT
 
     const removeLink = () => {
         const selection = editorState.getSelection()
-        updateStateForPopover(RichUtils.toggleLink(editorState, selection, null))
+        setEditorState(RichUtils.toggleLink(editorState, selection, null))
     }
 
     const confirmLink = (url?: string) => {
@@ -746,12 +746,8 @@ const MUIRichTextEditor: RefForwardingComponent<TMUIRichTextEditorRef, IMUIRichT
         if (!url) {
             if (urlKey) {
                 removeLink()
-                return
-            }
-            setState({
-                ...state,
-                anchorUrlPopover: undefined
-            })
+            } 
+            dismissPopover()
             return
         }
 
