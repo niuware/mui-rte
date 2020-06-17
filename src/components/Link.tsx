8d177eb178b19358380c30f8a844efc65ab1cb18
Erik Lopez <maileks@gmail.com>
2020-06-17 16:42:22 +0900

    Add className to anchor tag

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -8,7 +8,7 @@ type TLinkProps = {
 }
 
 const Link: FunctionComponent<TLinkProps> = (props) => {
-    const { url } = props.contentState.getEntity(props.entityKey).getData()
+    const { url, className } = props.contentState.getEntity(props.entityKey).getData()
     return (
         <a 
             href={url} 
@@ -16,7 +16,7 @@ const Link: FunctionComponent<TLinkProps> = (props) => {
                 textDecoration: "underline",
                 color: "inherit"
             }} 
-            className="editor-anchor"
+            className={`${className} editor-anchor`}
             target="_blank"
         >
             {props.children}
