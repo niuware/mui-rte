d9100ecf3a8230259a800458e4c941ef25a54e47
Adarsh Menon <adarsh1021@gmail.com>
2020-12-02 16:16:21 +0530

    prettier

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,59 +1,63 @@
-import React, { FunctionComponent } from 'react'
-import Paper from '@material-ui/core/Paper'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles'
+import React, { FunctionComponent } from "react";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 
 export type TAutocompleteItem = {
-    keys: string[]
-    value: any
-    content: string | JSX.Element
-}
+  keys: string[];
+  value: any;
+  content: string | JSX.Element;
+};
 
 interface TAutocompleteProps extends WithStyles<typeof styles> {
-    items: TAutocompleteItem[]
-    top: number
-    left: number
-    selectedIndex: number
-    onClick: (selectedIndex: number) => void
+  items: TAutocompleteItem[];
+  top: number;
+  left: number;
+  selectedIndex: number;
+  onClick: (selectedIndex: number) => void;
 }
 
-const styles = () => createStyles({
+const styles = () =>
+  createStyles({
     container: {
-        minWidth: "200px",
-        position: "absolute",
-        zIndex: 10
+      minWidth: "200px",
+      position: "absolute",
+      zIndex: 10,
     },
     item: {
-        cursor: "pointer"
-    }
-})
+      cursor: "pointer",
+    },
+  });
 
 const Autocomplete: FunctionComponent<TAutocompleteProps> = (props) => {
-    if (!props.items.length) {
-        return null
-    }
+  if (!props.items.length) {
+    return null;
+  }
 
-    const { classes } = props
-    return (
-        <Paper className={classes.container} style={{
-            top: props.top,
-            left: props.left
-        }}>
-            <List dense={true}>
-                {props.items.map((item, index) => (
-                    <ListItem
-                        key={index}
-                        className={classes.item}
-                        selected={index === props.selectedIndex}
-                        onClick={() => props.onClick(index)}
-                    >
-                        {item.content}
-                    </ListItem>
-                ))}
-            </List>
-        </Paper>
-    )
-}
+  const { classes } = props;
+  return (
+    <Paper
+      className={classes.container}
+      style={{
+        top: props.top,
+        left: props.left,
+      }}
+    >
+      <List dense={true}>
+        {props.items.map((item, index) => (
+          <ListItem
+            key={index}
+            className={classes.item}
+            selected={index === props.selectedIndex}
+            onClick={() => props.onClick(index)}
+          >
+            {item.content}
+          </ListItem>
+        ))}
+      </List>
+    </Paper>
+  );
+};
 
-export default withStyles(styles, { withTheme: true })(Autocomplete)
+export default withStyles(styles, { withTheme: true })(Autocomplete);
