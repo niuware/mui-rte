767adee2ec79903e0cb8b79175ef5b3e862d34de
Bastian Block <blockbasti99@gmail.com>
2020-12-03 10:40:32 +0100

    Support for React 17

--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,6 @@
 import 'jsdom-global/register'
 import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 
 Enzyme.configure({adapter: new Adapter()})
 const noop = () => {}
