d84f2ce1621601f4483135098d89e6d0dd58943a
Adarsh Menon <adarsh1021@gmail.com>
2020-12-04 13:20:07 +0530

    cleanup test

--- examples/floating-toolbar/index.tsx
+++ examples/floating-toolbar/index.tsx
@@ -1,16 +1,8 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import MUIRichTextEditor from "../../";
 
 const FloatingToolbar = () => {
   const [showToolbar, setShowToolbar] = useState(false);
-  const [pos, setPos] = useState({ bottom: 0, left: 0 });
-  const btnRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    setPos((p) => {
-      return { bottom: p.bottom + 0, left: p.left + 0 };
-    });
-  }, [showToolbar]);
 
   return (
     <>
@@ -18,11 +10,12 @@ const FloatingToolbar = () => {
         label="Type something here..."
         floatingToolbar={showToolbar}
         floatingToolbarControls={["bold", "italic", "link"]}
-        floatingToolbarPosition={pos}
+        floatingToolbarPosition={{
+          bottom: 0,
+          left: 0,
+        }}
       />
-      <button ref={btnRef} onClick={() => setShowToolbar(!showToolbar)}>
-        Toolbar
-      </button>
+      <button onClick={() => setShowToolbar(!showToolbar)}>Toolbar</button>
     </>
   );
 };
