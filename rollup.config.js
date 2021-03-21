27c92bc6387752649ae3338807bb8a4dcae106c4
Erik Lopez <maileks@gmail.com>
2021-03-21 15:50:43 +0900

    Update package entrypoint

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 
 export default {
-    input: './index.ts',
+    input: './src/MUIRichTextEditor.tsx',
     plugins: [
         typescript(),
         commonjs({
