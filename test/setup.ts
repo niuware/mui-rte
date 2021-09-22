f5b183d64c15429d01957224952d0a06044fdd48
Erik Lopez <maileks@gmail.com>
2021-09-22 20:53:16 +0900

    Remove tests

--- test/setup.ts
+++
@@ -1,7 +0,0 @@
-import 'jsdom-global/register'
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-Enzyme.configure({adapter: new Adapter()})
-const noop = () => {}
-Object.defineProperty(window, 'scrollTo', { value: noop, writable: true })
